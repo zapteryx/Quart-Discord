b96ac1c59aeccf949c5747d00af9071f81871375
Bobby Moretti <bobmoretti@gmail.com>
2020-10-06 19:32:56 -0700

    :bug: Create RateLimited exception using coroutines correctly.
    Add a coroutine creation classmethod to exceptions.RateLimited so that we can
    call the aiohttp.Response.to_json() and .text() methods and create an
    exception with the expected attributes.

--- quart_discord/exceptions.py
+++ quart_discord/exceptions.py
@@ -12,32 +12,43 @@ class RateLimited(HttpException):
 
     Attributes
     ----------
-    response : aiohttp.Response
-        The actual response object received from Discord API.
-    json : dict
-        The actual JSON data received. Shorthand to ``response.json()``.
+    resp_as_json : dict
+        The actual JSON data received. Value yielded from ``response.json()`` coroutine.
     message : str
         A message saying you are being rate limited.
     retry_after : int
         The number of milliseconds to wait before submitting another request.
     is_global : bool
         A value indicating if you are being globally rate limited or not
-
     """
 
-    def __init__(self, response):
-        self.response = response
+    def __init__(self, resp_as_json, message, is_global, retry_after):
+        self.resp_as_json = resp_as_json
+        self.message = message
+        self.is_global = is_global
+        self.retry_after = retry_after
+        super().__init__(self.message)
+
+    @classmethod
+    async def create(cls, response):
+        """Coroutine to convert an aiohttp response into a RateLimited exception.
+        
+        Attributes
+        ----------
+        response : aiohttp.Response
+            the aiohttp Response object.
+            
+        """
         try:
-            self.json = self.response.json()
+            resp_as_json = await response.json()
         except json.JSONDecodeError:
-            self.json = dict()
-            self.message = self.response.text
+            resp_as_json = dict()
+            message = await response.text()
         else:
-            self.message = self.json["message"]
-            self.is_global = self.json["global"]
-            self.retry_after = self.json["retry_after"]
-        finally:
-            super().__init__(self.message)
+            message = resp_as_json["message"]
+            is_global = resp_as_json["global"]
+            retry_after = resp_as_json["retry_after"]
+        return RateLimited(resp_as_json, message, is_global, retry_after)
 
 
 class Unauthorized(HttpException):
